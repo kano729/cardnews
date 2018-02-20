b9ac72dc12020d6123c8546c785a6db7cca4c029
imim10042 <orange10042@gmail.com>
2018-02-20 12:17:41 +0900

    ページ遷移機能追加

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import HotTable from 'react-handsontable'
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
 
   constructor() {
     super()
     this.state = {
-      articles: []
+      articles: [],
+      totalResults: 0,
+      page: 1,
+      count: [0]
     }
   }
 
   //コンポーネント生成時の挙動
   componentWillMount(){
-    this._onFetch()
+    this._onFetch(this.state.page)
   }
 
-  _onFetch = () => {
-    const url = 'https://newsapi.org/v2/everything?apiKey=2eab118a7c9d450299db63f95f6e0564&q=jcb OR visa OR クレジット OR プリペイド OR デビット'
+  _onFetch = (page) => {
+    console.log(page)
+    const url = 'https://newsapi.org/v2/everything?' +
+                'page=' + page +
+                '&apiKey=2eab118a7c9d450299db63f95f6e0564' +
+                '&q=jcb OR visa OR クレジット OR プリペイド OR デビット'
     fetch(url, {cache: false})
     .then(response => response.json())
     .then(json => {
       const articles = json.articles.map(article => article)
-      this.setState({articles})
+      const totalResults = json.totalResults
+      this.setState({articles,totalResults})
     })
 
   }
@@ -34,7 +44,7 @@ class App extends Component {
   }
 
   //カラムヘッダー定義
-  colHeaders = ['source', 'author', 'title', 'description', 'publishedAt']
+  colHeaders = ['source', 'author', 'title', 'description', 'image', 'publishedAt', 'like', 'select']
 
   //カラムデータ定義
   columns = [
@@ -42,10 +52,50 @@ class App extends Component {
     { data: 'author', editor: false },
     { data: 'title', editor: false, renderer: 'html' },
     { data: 'description', editor: false },
+    { data: 'image', editor: false, renderer: 'html' },
     { data: 'publishedAt', editor: false },
+    { data: 'like', editor: false },
+    { data: 'selected', type: 'checkbox' },
   ]
 
+  _onClick = () => {
+    console.log('Like!')
+  }
+
+  _prev = () => {
+    if(this.state.page !== 1) {
+      const page = this.state.page - 1
+      this.setState({page})
+      this._onFetch(page)
+    } else {
+      console.log('前のページはないよ')
+    }
+
+  }
+
+  _next = () => {
+    const page = this.state.page + 1
+    this.setState({page})
+    this._onFetch(page)
+  }
+
   render() {
+    let page = ""
+    // <button onClick={() => {this._prev()}}>前へ</button>
+    // <button onClick={() => {this._next()}}>次へ</button>
+    const style = {
+      margin: 12,
+    };
+    page = <div>page:{this.state.page}
+              <MuiThemeProvider>
+                <RaisedButton label="前へ" secondary={true} style={style} onClick={() => {this._prev()}}/>
+                <RaisedButton label="次へ" primary={true} style={style} onClick={() => {this._next()}}/>
+              </MuiThemeProvider>
+           </div>
+
+    let result = ""
+    result = <div>totalResults:{this.state.totalResults}</div>
+
     let message = ""
     if (this.state.articles !== []) {
       const _articles = this.state.articles.map(article => {
@@ -54,9 +104,13 @@ class App extends Component {
           author: article.author,
           title: "<a href='"+article.url+"'>"+article.title+"</a>",
           description: article.description,
+          image: "<img src='"+article.urlToImage+"'/>",
           publishedAt: article.publishedAt,
+          like: 0,
+          selected: false,
         }
-      }).sort((a, b) => {return a.publishedAt < b.publishedAt ? 1 : -1})
+      })
+//      }).sort((a, b) => {return a.publishedAt < b.publishedAt ? 1 : -1})
       message = <div style={this.styles.hot}><HotTable
                   root="hot"
                   data={_articles}
@@ -69,12 +123,18 @@ class App extends Component {
                   manualColumnResize={true}
                   /></div>
     } else {
+      page = "null"
+      result = "null"
       message = "null"
     }
     return (
       <div className="App">
         <h2>Card Payment Breaking News</h2>
         powered by <a href='https://newsapi.org/'>News API v2</a>
+        <br/>
+        <button onClick={() => {this._onClick()}}>Like!</button>
+        {page}
+        {result}
         {message}
       </div>
     );
